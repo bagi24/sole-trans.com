9d653f0e6105dfabd723e5fa8b5942a0e576ec8e
unknown <bagrat.injgia24@gmail.com>
2024-02-22 17:29:05 +0400

    added scrol function in css gallery component

--- src/components/aditionalService/AditionalService.jsx
+++ src/components/aditionalService/AditionalService.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import "./aditionalService.css";
 import ServicePhoto from "../../assets/sole/servise.jpg";
+import { useEffect, useState } from "react";
 
 const AditionalService = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const triggerPoint = 400; // Adjust the trigger point as needed
+
+      if (scrollTop > triggerPoint) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="services">
+    <div className={`services ${isVisible ? "visible" : ""}`}>
       <img className="servImg" src={ServicePhoto} alt="" />
     </div>
   );
--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./gallery.css";
-import photo1 from "../../assets/sole/104000559_2647762695549637_5907643460407411900_n.jpg";
+import photo1 from "../../assets/sole/292357204_3234318400227394_4066076448278349774_n.jpg";
 import photo2 from "../../assets/sole/137706535_2832897623702809_3799256577048642001_n.jpg";
 import photo3 from "../../assets/sole/26240057_1617592914962493_8115545812445165821_o.jpg";
 import photo5 from "../../assets/sole/258342550_3064412437217992_4642259824522359857_n.jpg";
