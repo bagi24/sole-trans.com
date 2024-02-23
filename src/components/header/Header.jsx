0fcf2e4fe5aab2dad2c942aac7023d26839a893f
unknown <bagrat.injgia24@gmail.com>
2024-02-23 13:29:14 +0400

    added language optional for navigation component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import './header.css';
-import { Link } from 'react-router-dom';
-import CompanyLogo from '../../assets/logo.jpg';
-import { useState } from 'react';
+import React from "react";
+import "./header.css";
+import { Link } from "react-router-dom";
+import CompanyLogo from "../../assets/logo.jpg";
+import { useState } from "react";
+import GeFlag from "../../assets/sole/GE-Georgia-Flag-icon.png";
+import EnFlag from "../../assets/sole/EN-English.jpg";
+import RuFlag from "../../assets/sole/RU-Russia.png";
 
 const Header = () => {
   const [isHovered, setIsHovered] = useState(false);
   const [isHoveredAbout, setIsHoveredAbout] = useState(false);
+  const [isHoveredFlag, setIsHoveredFlag] = useState(false);
   const [active, setActive] = useState(false);
 
   const openModal = () => {
@@ -18,46 +22,57 @@ const Header = () => {
   };
 
   return (
-    <header className='header'>
-      <div className='container '>
-        <nav className='navbar'>
-          <Link to='/' className='logo'>
-            <img src={CompanyLogo} alt='' />
-            <span className='logoSpan'>OLE-TRANS</span>
+    <header className="header">
+      <div className="container ">
+        <nav className="navbar">
+          <Link to="/" className="logo">
+            <img src={CompanyLogo} alt="" />
+            <span className="logoSpan">OLE-TRANS</span>
           </Link>
-          <h1 className='sole'>
-            <i class='fa-solid fa-truck'></i>
+          <h1 className="sole">
+            <i class="fa-solid fa-truck"></i>
           </h1>
-          <ul className='nav-menu'>
+          <ul className="nav-menu">
             <li
-              className='servicess'
+              className="servicess"
               onMouseEnter={() => setIsHovered(true)}
-              onMouseLeave={() => setIsHovered(false)}>
-              <Link to='/services'>Services</Link>
+              onMouseLeave={() => setIsHovered(false)}
+            >
+              <Link to="/services">Services</Link>
             </li>
             <li
               onMouseEnter={() => setIsHoveredAbout(true)}
-              onMouseLeave={() => setIsHoveredAbout(false)}>
-              <Link to='/about'>About Us</Link>
+              onMouseLeave={() => setIsHoveredAbout(false)}
+            >
+              <Link to="/about">About Us</Link>
             </li>
             <li>
-              <Link to='/contact'>Contact</Link>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li
+              onMouseEnter={() => setIsHoveredFlag(true)}
+              onMouseLeave={() => setIsHoveredFlag(false)}
+            >
+              <Link to="/en">
+                <img className="GeFlag" src={GeFlag} alt="" />
+              </Link>
             </li>
           </ul>
         </nav>
       </div>
       {isHovered && (
         <ul
-          className='optional'
+          className="optional"
           onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}>
-          <Link className='link-style' to='/aboutUs'>
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          <Link className="link-style" to="/aboutUs">
             <li>საერთაშორისო </li>
           </Link>
-          <Link className='link-style' to='/blog'>
+          <Link className="link-style" to="/blog">
             <li>სახმელეთო</li>
           </Link>
-          <Link className='link-style' to='/blog'>
+          <Link className="link-style" to="/blog">
             <li>შიდა</li>
           </Link>
         </ul>
@@ -65,14 +80,34 @@ const Header = () => {
 
       {isHoveredAbout && (
         <ul
-          className='optionalAbout'
+          className="optionalAbout"
           onMouseEnter={() => setIsHoveredAbout(true)}
-          onMouseLeave={() => setIsHoveredAbout(false)}>
-          <Link className='link-style' to='/aboutUs'>
+          onMouseLeave={() => setIsHoveredAbout(false)}
+        >
+          <Link className="link-style" to="/aboutUs">
             <li>ისტორია </li>
           </Link>
-          <Link className='link-style' to='/blog'>
+          <Link className="link-style" to="/blog">
             <li>პარტნიორები</li>
+          </Link>
+        </ul>
+      )}
+
+      {isHoveredFlag && (
+        <ul
+          className="optionalFlag"
+          onMouseEnter={() => setIsHoveredFlag(true)}
+          onMouseLeave={() => setIsHoveredFlag(false)}
+        >
+          <Link className="link-style" to="/blog">
+            <li>
+              <img className="flag" src={EnFlag} alt="" />
+            </li>
+          </Link>
+          <Link className="link-style" to="/blog">
+            <li>
+              <img className="flag" src={RuFlag} alt="" />
+            </li>
           </Link>
         </ul>
       )}
